5e36bc07ef6ffb58ce908567daa1830d0781db71
Prasad M <prasadm@systechusa.com>
2024-10-08 09:15:02 +0000

    OKAY

--- models/example/Scoring.sql
+++ models/example/Scoring.sql
@@ -6,10 +6,10 @@
 
 WITH Scoring as 
 (select
-coalesce(playerid,'NA'),
-coalesce(year,9999),
-coalesce(POS,'NA'),
-COALESCE(GP,0)
+coalesce(playerid,'NA') playerid,
+coalesce(year,9999) year,
+coalesce(POS,'NA')POS,
+COALESCE(GP,0) GP
 from {{ source('Hockey_Table', 'SCORING') }})
 
 select * from scoring
