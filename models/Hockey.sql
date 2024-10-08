413836ea6d9526887723c855ab39c442b0bc1587
Prasad M <prasadm@systechusa.com>
2024-10-08 07:41:05 +0000

    Commit

--- models/Hockey.sql
+++ models/Hockey.sql
@@ -7,9 +7,8 @@
 with Hockey_teams as (
     select name,g,year
     from hockeydb.hockey.teams
-    limit 5
    )
 
-select  * from Hockey_teams order by name ,g
+select  * from Hockey_teams 
 
 
--- models/example/dbt_top5_team.sql
+++ models/example/dbt_top5_team.sql
@@ -5,8 +5,8 @@
 }}
 
 with top_5 as (
-    select name,  MAX(g) OVER (PARTITION BY g) as no_of_goals, year 
-    from {{ ref('Hockey') }} 
+    select name,  MAX(g) OVER (PARTITION BY g ORDER BY g desc) as no_of_goals, year 
+    from {{ source("Hockey_Table", "TEAMS") }}  
     where year = '2011'
      limit 5
 )
