fad00e9a9b1f47670ded45bae8dd81b0cc070e04
Prasad M <prasadm@systechusa.com>
2024-10-08 12:27:22 +0000

    ok

--- models/example/Coaches_incremental.sql
+++ models/example/Coaches_incremental.sql
@@ -8,4 +8,7 @@ WITH Coaches_incremental as (
 {{ source('Hockey_Table', 'COACHES')
   }})
 
-select * from Coaches_incremental
+ select * from Coaches_incremental 
+
+select  COACHID,year,STINT,count(*) from Coaches_incremental
+ group by 1,2,3 having count(*)=1
