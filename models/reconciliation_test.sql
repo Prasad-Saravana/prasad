f7002715e5a56f967589c5a1d068a20704d6788b
Prasad M <saravanaprasad0115@gmail.com>
2024-10-15 08:52:49 +0000

    ok

--- models/reconciliation_test.sql
+++ models/reconciliation_test.sql
@@ -1,14 +1,15 @@
-with reconciliation as (
+with reconciliation_count as (
     select
-        (select count(*) from {{ ref('reconciliation') }}) as total_records,
-        (select count(*) from {{ ref('reconciliation') }} where missing_in_target_count > 0) as missing_records
+        (select source_count from {{ ref('reconciliation') }}) as Source_records,
+        (select target_count from {{ ref('reconciliation') }}) as Target_records,
+        (select missing_in_target_count from {{ ref('reconciliation') }} ) as missing_records
 )
- 
 select
-    total_records,
+    Source_records,
+    Target_records,
     missing_records,
     case
         when missing_records = 0 then 'Success: No missing_records found'
         else 'Failure: missing_records detected'
     end as result
-from reconciliation
+from reconciliation_count
